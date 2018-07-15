0837cefa0d0719736c83652e6f08c0b8fb3d6ba8
EdgeWasherMcgee <axel.ingvast@gmail.com>
2018-07-15 19:37:18 +0200

    Changed array-logic in sudokuM. Added logic-support to the GUI

--- sudokuM.py
+++ sudokuM.py
@@ -2,49 +2,52 @@ import random
 
 class sudM():
 
+    def __init__(self):
+        self.gridG()
+
     def gridG(self): #Returns a suduko with some weird numbers inside
-        self.m = [[[str(varx+vary+whole) for varx in range(3)] for vary in range(3)] for whole in range(9)]
+        self.m = [0 for y in range(81)]
 
     def pickR(self, row): #Returns the given row
         result = []
-        l = {1 : [0,3], 2 : [0,3], 3 : [0,3], 4 : [3,6], 5 : [3, 6], 6 : [3, 6], 7 : [6, 9], 8 : [6, 9], 9 : [6, 9]}
-        for outL in self.m[l[row][0]:l[row][1]]:
-            result.append(self.m[outL][row])
+        for y in range(3):
+            for x in range(3):
+                result.append(self.m[y * 9 + x + int(row / 3) * 27 + (row % 3) * 3])
         return result
 
     def pickC(self, col): #Returns the given colunmn
         result = []
-        for outC in range(0,9,3):
-            for outIC in self.m:
-                result.append(outIC[int(col/3)][col%3])
+        for y in range(3):
+            for x in range(3):
+                result.append(self.m[y * 27 + x * 3 + col % 3 + int(col / 3) * 9])
         return result
 
-
     def idS(self, s): #Identifies which row, column and 3x3-square the square is in
-        r = int(s[0]/3) * 3 + s[1]
-        c = (s[0]%3 * 3) + s[2]
-        sq = sudM.m[s[0]]
+        sq = int(s/9)
+        r = int((s % 9) / 3) + int(sq / 3) * 3
+        c = (s % 9) % 3 + (sq % 3) * 3
+
         return r, c, sq
 
     def checkS(self, s): #Checks which numbers are available
-        r, c = sudM.idS(s)
-        rI = sudM.pickR(r)
-        cI =  sudM.pickC(c)
+        r, c, sq = self.idS(s)
+        rI = self.pickR(r)
+        cI =  self.pickC(c)
         false = False
+        print(rI, cI)
         true = []
-        for n in range(1,11):
+        for n in range(0,10):
             if n in rI:
-                false = False
+                pass
             elif n in cI:
-                false = False
-            elif n in m[s[0]]:
-                false = False
+                pass
+            elif n in self.m[sq * 9: sq * 9 + 9]:
+                pass
             else:
                 true.append(n)
         return true
 
-    def switchP(self, s):
-        r, c, sq = sudM.idS(s)
-        val = sudM.m[s[0]][s[1]][s[2]]
+    def setS(self, s, inf):
+        self.m[s] = int(inf)
 
 
--- tktest.py
+++ tktest.py
@@ -1,30 +1,39 @@
 import tkinter as tk
+import sudokuM
 
 
 class sudokuTk():
 
     def __init__(self):  #Initializiing the GUI, making a root and calling the necessary functions
+        self.sM = sudokuM.sudM()
         self.root = tk.Tk()
-        self.targetFrame = 0
-        self.shell = []
+        self.targetCell = 0  #A variable that will always change to the coordination that the mouse hovers over
+        self.shell = []  #The grid-system that sudoku is built upon
+        self.labels = []  #The labels that the grid-system will contain
         self.mkGrid()
         self.mkLabels()
         self.bindFrames()
         self.rootKeyInput()
+        self.informBar()
         self.root.mainloop()
 
+    def informBar(self):  #Creates a frame that acts like an information section that lets you know what you've done bad
+        self.informationBar = tk.Frame(self.root, height = 15, width = 280, pady = 3,
+                                       background = '#aaa')
+        self.informationBar.grid(column = 0, row = 1)
+
     def mkLabels(self):  #Makes a label in every cell of the grid and also assigns it a variable that may be changed
-        self.labels = [['i1', str(cell)]for cell in range(81)]
+        self.labels = [['', str(cell)]for cell in range(81)]
         for label in range(81):
             self.labels[label][0] = tk.Label(self.shell[label], textvariable = self.labels[label][1])
-            self.labels[label][0].grid()
+            self.labels[label][0].place(relx = 0.5, rely = 0.5, anchor = tk.CENTER)
 
-
-
-    def mkGrid(self): #Makes a sudoku grid of frames, later to have Labels stored in them and be binded to functions
+    def mkGrid(self):  #Makes a sudoku grid of frames, later to have Labels stored in them and be binded to functions
+        self.gridFrame = tk.Frame(self.root)
+        self.gridFrame.grid()
         for varx in range(3):
             for vary in range(3):
-                temp = tk.Frame(self.root, bd = 2, relief = "ridge", height = 300, width = 300, )
+                temp = tk.Frame(self.gridFrame, bd = 2, relief = "ridge", height = 300, width = 300, )
                 temp.grid(row = varx, column = vary)
                 for whole in range(9):
                     cell = tk.Frame(temp, bd=1, relief="ridge", height=30, width=30)
@@ -32,22 +41,32 @@ class sudokuTk():
                     cell.grid(row=int(whole / 3), column=whole % 3)
                     cell.grid_propagate(0)
 
-    def bindFrames(self): #Binds all the frames with Enter and Leave functions to make them interactive
+    def bindFrames(self):  #Binds all the frames with Enter, and if you enter they redirect them to focusCell
         for frame in range(len(self.shell)):
             self.shell[frame].bind('<Enter>', lambda event, frame = frame: self.focusCell(event, frame))
 
-    def focusCell(self, event, frame):
-        self.targetFrame = frame
+    def focusCell(self, event, frame):  #Makes a global variable telling which square you're hovering over.
+        self.targetCell = frame
 
-    def rootKeyInput(self):
-        self.root.bind('<Key>', lambda event: self.labelAFrame(event))
+    def rootKeyInput(self):  #Binds key input to root, used to assign squares text
+        self.root.bind('<Key>', lambda event: self.contrInput(event))
 
-    def labelAFrame(self, event):
-        numbers = [str(y) for y in range(1,10)]
+    def contrInput(self, event):  #Checks if the input is legal sudoku-vise
+        print(self.sM.checkS(self.targetCell), self.targetCell, event.char)
+
         if event.keysym == 'BackSpace':
-            self.labels[self.targetFrame][0].setvar(self.labels[self.targetFrame][1], ' ')
+            self.labelAFrame(event)
+        elif int(event.char) in self.sM.checkS(self.targetCell):
+            self.labelAFrame(event)
+
+    def labelAFrame(self, event):  #If the keyboard input is appropriate, put it on the square that the mouse is over
+        numbers = [str(y) for y in range(1, 10)]
+        if event.keysym == 'BackSpace':
+            self.labels[self.targetCell][0].setvar(self.labels[self.targetCell][1], ' ')
+            self.sM.setS(self.targetCell, 0)
         elif event.char in numbers:
-            self.labels[self.targetFrame][0].setvar(self.labels[self.targetFrame][1], event.char)
+            self.labels[self.targetCell][0].setvar(self.labels[self.targetCell][1], event.char)
+            self.sM.setS(self.targetCell, event.char)
 
 
 a = sudokuTk()
